6eb688ee8ecb1372219c140c6fb05b9fd5c0acfc
Sanjay Yadav <sanjaykumar5c9@yahoo.com>
2017-09-15 23:05:28 +0530

    Flash implemented.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
+public flashstatus;
+public flashvalue;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private flashlight: Flashlight) {
+  	this.flashstatus = 'off';
+    this.flashvalue = 1;
+  	this.flashlight.switchOn();
   }
+
+  LightOn() {
+        flashlight.available((isAvailable) => {
+            if(isAvailable)
+                flashlight.toggle();
+                this.flashvalue += 1;
+            if(this.flashvalue % 2 != 0)
+                this.flashstatus = 'on';
+            else
+                this.flashstatus = 'off';
+            
+        })
+    }
 
 }
